4b71a57a86f359d0301634bdb315fc5f87f38314
Renee Thomsen <chuhan.thomsen@collabera.com>
2021-03-15 22:01:37 -0400

    updated AT project

---
+++ AT/src/app/course.service.ts
@@ -0,0 +1,18 @@
+import { MessagesService } from './messages.service';
+import { COURSES } from './mock-courses';
+import { Course } from './course';
+import { Injectable } from '@angular/core';
+import { Observable, of} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService {
+  
+  constructor(private messagesService: MessagesService) { }
+
+  getCourses(): Observable<Course[]> {
+    this.messagesService.add('CourseService: fetched course');
+    return of(COURSES);
+  }
+}
